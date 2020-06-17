d789da18eda8e7ef3cb6eaa7c33c2c6689eebe55
jnanavelamuri <velamurij24@greenhill.org>
2020-06-17 11:06:53 -0500

    updating

--- src/com/company/datatype.java
+++ src/com/company/datatype.java
@@ -12,7 +12,7 @@ print a new line with the original s added with the persons chosen variable
  */
 package com.company;
 import java.util.Scanner;
-public class data_type_challenge {
+public class datatype {
 
     public static void main(String[] args) {
 
@@ -20,11 +20,11 @@ public class data_type_challenge {
         double d= 25.0;
         String s= "happy birthday ";
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter integer");
+        System.out.println("enter integer:");
         int myi = scan.nextInt();
-        System.out.println("enter double");
+        System.out.println("enter double:");
         double myd = scan.nextDouble();
-        System.out.println("enter string");
+        System.out.println("enter string:");
         String mys = scan.next();
         System.out.printf("%d \n ", i + myi);
         System.out.printf("%10.1f \n ", d + myd);
