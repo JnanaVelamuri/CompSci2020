1d0956e2555ab1253a54039ad0426df2e444990a
jnanavelamuri <velamurij24@greenhill.org>
2020-06-21 11:32:54 -0500

    updating

--- src/com/company/stringschallenge.java
+++ src/com/company/stringschallenge.java
@@ -8,7 +8,28 @@ if the number is odd it goes into another group
 println with the even group and then a space and then with the odd group
  */
 package com.company;
-
+import java.util.Scanner;
 public class stringschallenge {
+public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    String userWord;
+    System.out.println("please enter a word:");
+   userWord = sc.next();
+    String newWord = scramble(userWord);
+System.out.println(newWord);
+}
+public static String scramble (String userWord) {
+    String evenWord= "";
+    String oddWord= "";
 
+    for (int even=0; even<userWord.length(); even += 2) {
+        char temp= userWord.charAt(even);
+        evenWord = evenWord+temp;
+    }
+    for (int odd=1; odd<userWord.length(); odd += 2) {
+        char temp = userWord.charAt(odd);
+        oddWord = oddWord + temp;
+    }
+    return (evenWord+ " "+oddWord);
+}
 }
