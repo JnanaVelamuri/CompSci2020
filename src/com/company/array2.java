e09a80845ab745b66bafe49216bc666a2d47e664
jnanavelamuri <velamurij24@greenhill.org>
2020-06-23 13:54:26 -0500

    updating

--- src/com/company/array2.java
+++ src/com/company/array2.java
@@ -19,29 +19,28 @@ package com.company;
 import java.util.*;
 public class array2 {
     public static void main(String[] args) {
-        Random randy = new Random();
-        int num1 = randy.nextInt(5)+5;
-        int[] myArray = new int[num1];
+        Random randy = new Random();// for the random
+        int num1 = randy.nextInt(5)+5;// makes the amount of numbers random
+        int[] myArray = new int[num1];//
         for (int i = 0; i < num1; i++) {
-            myArray[i] = randy.nextInt(95) + 5;
+            myArray[i] = randy.nextInt(95) + 5;// makes the random numbers not over 100 but not under 5
         }
             int index1;
             int index2;
             do{
-                index1=randy.nextInt(num1);
-                index2=randy.nextInt(num1);
+                index1=randy.nextInt(num1);// chooses a random number randomly
+                index2=randy.nextInt(num1);// chooses a random number randomly
             }while (index2== index1);
-        System.out.println(Arrays.toString(myArray));
+        System.out.println(Arrays.toString(myArray));// prints out the numbers before the swap
         swap(myArray,index1,index2);
-        System.out.println(Arrays.toString(myArray));
 
     }
-    public static int[] swap (int[]  swap,int index1, int index2) {
+    public static int[] swap (int[]  swap,int index1, int index2) {// switches the two randomly chosen random numbers
         int temp = swap[index1];
         swap [index1]= swap [index2];
         swap[index2]=temp;
 
-        System.out.println(Arrays.toString(swap));
+        System.out.println(Arrays.toString(swap));// prints out the numbers after the swap
 
         return swap;
     }
