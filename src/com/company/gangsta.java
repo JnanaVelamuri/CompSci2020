ef7d3c2ebe7f8459e8853a53e380eed6d5157807
jnanavelamuri <velamurij24@greenhill.org>
2020-06-22 22:19:49 -0500

    updating

--- src/com/company/gangsta.java
+++ src/com/company/gangsta.java
@@ -10,12 +10,12 @@ public class gangsta {
 
     public static void sayHI(String name) {
         int indexOf = name.indexOf(" ");
-        char initial1= name.charAt(0);
-            String lastname = name.substring(indexOf + 1);
-            lastname = lastname.toUpperCase();
-            String firstname=name.substring (0,indexOf);
-            String gangstaname= initial1 + ".Diddy "+ lastname+" "+firstname + "-izzle";
-            System.out.print("Your gangsta name is:"+ gangstaname);
+        char initial1= name.charAt(0);// identifies the first letter of the fully name
+            String lastname = name.substring(indexOf + 1);// finds the second name
+            lastname = lastname.toUpperCase();// makes the last name upper cased
+            String firstname=name.substring (0,indexOf);// finds the first name
+            String gangstaname= initial1 + ".Diddy "+ lastname+" "+firstname + "-izzle";// puts the first letter, diddy, the last name (which is upper cased), the first name, and izzle all together
+            System.out.print("Your gangsta name is:"+ gangstaname);// prints out the final answer
 
         }
     }
