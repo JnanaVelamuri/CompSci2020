90d54054eabb4f4a0edc4046be80901047acf819
jnanavelamuri <velamurij24@greenhill.org>
2020-06-24 12:27:51 -0500

    updating

---
+++ src/com/company/filetesting.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+public class filetesting {
+public static void main (String [] args) throws FileNotFoundException {
+    File file = new File("");
+    Scanner input = new Scanner(file);
+    int number;
+    while (input.hasNext()) {
+
+    }
+    }
+}
